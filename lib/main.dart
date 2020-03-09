10076378b6f931bd70ed538691e7b908ddc1cc5a
adnan morshed <adnan.starit118@gmail.com>
2020-03-09 18:15:06 +0600

    ok testing...

--- lib/main.dart
+++ lib/main.dart
@@ -55,5 +55,6 @@ class MyDoctorApps extends StatelessWidget {
     SharedPreferences themsSP = await SharedPreferences.getInstance();
     print('thems Bool ' + themsSP.getBool('isThems').toString());
     darkTheme = themsSP.getBool('isThems');
+    print('Testing File done ');
   }
 }
